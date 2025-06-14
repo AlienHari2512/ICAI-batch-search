54b08a7415c4abdeb60de42f3cc3600d729628a5
AlienHari2512 <hariharasudhan0205@gmail.com>
2025-06-14 11:36:36 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+{
+  "name": "discord-text-check-bot",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.13.0",
+    "puppeteer": "^21.3.8",
+    "dotenv": "^16.4.5",
+    "node-cron": "^3.0.3"
+  }
+}
